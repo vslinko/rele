62fb765b68bb0a707e9ee5e8adbc80829c99482e
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-05-14 13:48:59 +0300

    Remove test ql from example

--- example/src/index.js
+++ example/src/index.js
@@ -2,13 +2,6 @@ import 'whatwg-fetch';
 import React from 'react';
 import App from './components/App';
 
-import {ql} from '../../lib/ql'
-
-ql`
-User {
-}
-`;
-
 document.addEventListener('DOMContentLoaded', () => {
   React.render(<App />, document.getElementById('app'));
 });
