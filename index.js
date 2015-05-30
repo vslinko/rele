7b4b9efe75d3191486c6cb740b84ae862cb45c85
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-05-30 18:59:12 +0300

    Expose Actions and Store classes from flummox

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 module.exports.Rele = require('./dist/Rele');
+module.exports.Actions = require('flummox').Actions;
+module.exports.Store = require('flummox').Store;
 module.exports.releAction = require('./dist/util/releAction');
 module.exports.ql = require('./dist/ql').ql;
