ebe639979d409516a7786b9d1581969ce855e7f9
Vyacheslav Slinko <vyacheslav.slinko@gmail.com>
2015-05-30 15:00:07 +0300

    Declarative actions

--- example/src/flux/releActions.js
+++ example/src/flux/releActions.js
@@ -1,34 +1,56 @@
 import {releAction} from '../../..';
 import fetchJsonApi from '../utils/fetchJsonApi';
 
-export const category = releAction({
-  fetch({id}, fields, include, parent, flux) {
-    return fetchJsonApi(`/api/categories/${id}`, fields, include);
-  },
+function singleResourceAction({url, type}) {
+  return releAction({
+    fetch({id}, fields, include, parent, flux) {
+      return fetchJsonApi(url.replace(':id', id), fields, include);
+    },
 
-  filter({id}, parent, store, flux) {
-    return store.getResource('Category', id);
-  }
+    filter({id}, parent, store, flux) {
+      return store.getResource(type, id);
+    }
+  })
+}
+
+function collectionAction({url, type, parent = {}}) {
+  return releAction({
+    fetch({}, fields, include, parentResource, flux) {
+      let params = [];
+
+      if (parentResource && parent[parentResource.type]) {
+        const paramsFn = parent[parentResource.type].params;
+        params = params.concat(paramsFn(parentResource));
+      }
+
+      return fetchJsonApi(url, fields, include, params);
+    },
+
+    filter({}, parentResource, store, flux) {
+      let resources = store.get(type);
+
+      if (parentResource && parent[parentResource.type]) {
+        const filter = parent[parentResource.type].filter;
+        resources = resources.filter(resource => filter(resource, parentResource));
+      }
+
+      return resources;
+    }
+  });
+}
+
+export const category = singleResourceAction({
+  url: '/api/categories/:id',
+  type: 'Category'
 });
 
-export const items = releAction({
-  fetch({}, fields, include, parent, flux) {
-    const params = [];
-
-    if (parent && parent.type === 'Category') {
-      params.push(`filter[category]=${parent.id}`);
+export const items = collectionAction({
+  url: '/api/items',
+  type: 'Item',
+  parent: {
+    Category: {
+      params: category => `filter[category]=${category.id}`,
+      filter: (item, category) => item.relationships.category.data.id === category.id
     }
-
-    return fetchJsonApi(`/api/items`, fields, include, params);
-  },
-
-  filter({}, parent, store, flux) {
-    let items = store.get('Item');
-
-    if (parent && parent.type === 'Category') {
-      items = items.filter(item => item.relationships.category.data.id === parent.id);
-    }
-
-    return items;
   }
 });
